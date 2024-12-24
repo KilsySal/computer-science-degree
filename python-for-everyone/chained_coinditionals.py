cabcf42754e76850550cb0785cc29a25e9e7b2ca
Kilsy Salazar <kilsysalazar@outlook.com>
2024-12-23 19:50:56 -0500

    Organinig more files

---
+++ python-for-everyone/chained_coinditionals.py
@@ -0,0 +1,18 @@
+x = input('Enter value:\n')
+y = input('Enter value:\n')
+
+if x < y:
+    print('x smaler than y')
+elif x > y:
+    print("x greater than y")
+else:
+    print('There are equal!')
+
+""" 
+in order to get permition to execute a file 
+I choud use the following conmand:
+                        chmod +x file name 
+
+Toview files permitions tipe:
+ls -l
+"""
--- python-for-everyone/finding_bigest_num.py
+++ python-for-everyone/finding_bigest_num.py
@@ -10,16 +10,3 @@ def bigestNumber(numbers):
 
 # To call the funtion
 print(bigestNumber([374, 3647, 65748, 647546, 2, 4, 857]))
-
-tot = 0
-for i in [5, 4, 3, 2, 1]:
-    tot = tot + 1
-print(tot)
-
-zork = 0
-for thing in [9, 41, 12, 3, 74, 15]:
-    zork = zork + thing
-print('After', zork)
-
-if smallest is None:
-    smallest = value
